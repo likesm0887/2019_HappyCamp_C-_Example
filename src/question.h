3227664c80656b55524adcfeae7dc4942c87935c
YuXin Chen <likesm0857@gmail.com>
2019-08-23 22:16:22 +0800

    排版

--- src/question.h
+++ src/question.h
@@ -1,14 +1,14 @@
-class Question{
+class Question {
 public:
-Question(int index, String question, String answer, int score ){
-    _index = index;
-    _q = question;
-    _a = answer;
-    _score = score;
-}
-int _index;
+	Question(int index, String question, String answer, int score) {
+		_index = index;
+		_q = question;
+		_a = answer;
+		_score = score;
+	}
+	int _index;
 private:
-String _q;
-String _a;
-int _score;
+	String _q;
+	String _a;
+	int _score;
 };
--- src/quiz.h
+++ src/quiz.h
@@ -1,37 +1,37 @@
 #include <vector>
 #include "question.h"
 
-class Quiz{
+class Quiz {
 public:
-Quiz();
-void setQuestion(Question question){
-    questionVector.push_back(question);
+	Quiz();
+	void setQuestion(Question question) {
+		questionVector.push_back(question);
 
-}
-int scoreCal( ){
-    Iterator it = questionVector.begin();
-    int totalScore ;
-    int temp = 0;
-    for (it!= questionVector.end();++it)
-    {
-        if(it.index % 2 == 0) 
-        {
-            temp = *it.getScore()*2.5; 
-        }
-        else
-        {
-            temp = *it.getScore();
-        }
-        totalScore += temp;
-    }
-    return totalScore;
-}
+	}
+	int scoreCal() {
+		Iterator it = questionVector.begin();
+		int totalScore;
+		int temp = 0;
+		for (it != questionVector.end(); ++it)
+		{
+			if (it.index % 2 == 0)
+			{
+				temp = *it.getScore()*2.5;
+			}
+			else
+			{
+				temp = *it.getScore();
+			}
+			totalScore += temp;
+		}
+		return totalScore;
+	}
 
-void changeQuestionScore(Question q ,String  score)
-{
-    q.setQuestion(score);
-}
+	void changeQuestionScore(Question q, String  score)
+	{
+		q.setQuestion(score);
+	}
 
 private:
-vector<Question* > questionVector;
+	vector<Question* > questionVector;
 };
